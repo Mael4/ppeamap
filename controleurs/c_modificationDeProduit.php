ebf5ee65f6e980b6cb5f80580144c367e7a646a5
Mael <mael.berrue@gmail.com>
2018-01-18 12:31:50 +0100

    Ajout back office modification d'un produit producteur(tout sauf images à finir)

--- controleurs/c_modificationDeProduit.php
+++ controleurs/c_modificationDeProduit.php
@@ -1,6 +1,38 @@
 <?php
+$categories = get_categ();
 	
+	if (!isset($_REQUEST['categ']))
+	{
+		$produits = get_produit(0);
+	}
+	
+	else
+	{
+		$produits = get_produit($_REQUEST['categ']);
+	}
 	include('vues/v_modificationDeProduit.php');
-	
+
+
+	$libelle=$_POST['libelle'];
+        $prix=$_POST['prix'];
+        $qtt=$_POST['qtt'];
+        $description=$_POST['description'];
+        $id=$_POST['id'];
+         global $bdd;
+        $req = $bdd->prepare("UPDATE produit SET libelle='".$libelle."',
+                                             prixunitaire=".$prix.",
+                                             quantite=".$qtt.",
+                                             description='".$description."'
+                                             WHERE id=".$id."");
+      
+              
+           
+         try{
+        $req->execute();
+       
+         }
+      
+         catch(ErrorException $e){};
+
 ?>
 
--- util/connexion_sql.php
+++ util/connexion_sql.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion bdd
 try
-{$bdd = new PDO('mysql:host=localhost;dbname=bdberrue', 'root');}
+{$bdd = new PDO('mysql:host=localhost;dbname=bdberrue;charset=utf8','root','');}
 catch(Exception $e)
 {die('Erreur : '.$e->getMessage());}
